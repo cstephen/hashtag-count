89e29463b63e6f5f9f5f7b683e9a652a4a2f3d0b
Craig Stephenson <crstephenson@alaska.edu>
2017-01-11 05:48:41 -0900

    Initial commit of basic functionality.

---
+++ example.js
@@ -0,0 +1,44 @@
+var async = require('async');
+var tallytweets = require('./tallytweets')
+
+// Log into your Twitter account and go here https://apps.twitter.com/
+// to generate keys for your application.
+var tallytweets = tallytweets({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+});
+
+// Array of terms to tally. Can be anything, such as @people or just plain
+// words, but hashtags are better for observing trends.
+var terms = [ '#superbowl', '#pizza', '#beer' ];
+
+// Time interval in seconds. The tally for each time interval will be pushed
+// into the term's array in the output object.
+var interval = 15;
+
+// An object full of the provided terms and their associated time interval
+// tally array. Looks like this:
+// output = {
+//   '#superbowl': [ 7, 12, 4, 9, 2 ],
+//   '#pizza': [ 3, 1, 4, 2, 5 ],
+//   '#beer': [ 4, 9, 7, 2, 1 ]
+// }
+var output = {};
+
+// Open a connection to Twitter's Steaming API and start capturing tweets!
+tallytweets.start(terms, interval, output);
+
+// Confirm that results are being updated as expected.
+async.forever(
+  function(next) {
+    setTimeout(function () {
+      console.log(output);
+      next();
+    }, interval * 1000);
+  },
+  function(err) {
+
+  }
+);
---
+++ tallytweets.js
@@ -0,0 +1,65 @@
+var async = require('async');
+var Twit = require('twit');
+
+module.exports = function (keys) {
+  var T = new Twit({
+    consumer_key:         keys.consumer_key,
+    consumer_secret:      keys.consumer_secret,
+    access_token:         keys.access_token,
+    access_token_secret:  keys.access_token_secret,
+    timeout_ms:           60 * 1000,
+  });
+
+  var stream;
+  var startTime;
+  var interval;
+  var terms = [];
+  var output;
+  var tally = {};
+
+  function start(t, i, o) {
+    terms = t.slice(0);
+    interval = i;
+    output = o;
+
+    stream = T.stream('statuses/filter', { track: terms });
+
+    for(var i = 0; i < terms.length; i++) {
+      output[terms[i]] = [];
+    }
+
+    stream.on('tweet', function (tweet) {
+      for(var i = 0; i < terms.length; i++) {
+        if(tweet.text.indexOf(terms[i]) !== -1) {
+          tally[terms[i]] += 1;
+        }
+      }
+    });
+
+    async.forever(
+      function(next) {
+        resetCount();
+        setTimeout(function () {
+          for(var i = 0; i < terms.length; i++) {
+            output[terms[i]].push(tally[terms[i]]);
+          }
+          next();
+        }, interval * 1000);
+      },
+      function(err) {
+
+      }
+    );
+  }
+
+  function resetCount() {
+    for(var i = 0; i < terms.length; i++) {
+      tally[terms[i]] = 0;
+    }
+  }
+
+  // Export these functions.
+  return {
+    start: start
+  };
+}
